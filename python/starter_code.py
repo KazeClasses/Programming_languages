1d9f0b314b91d2ad633ab53900236f4e3da06c29
kazewong <kazewong.physics@gmail.com>
2024-07-11 12:19:35 -0400

    Updates starter code

--- python/starter_code.py
+++ python/starter_code.py
@@ -4,14 +4,18 @@ import time
 N = 100
 random.seed(int(time.time()))
 
+
 def insertion_sort(unsorted_list: list[int]) -> list[int]:
     # Implement your function here ...
     raise NotImplementedError
 
+
+# Do not change the following lines
 if __name__ == "__main__":
     input_list = list(range(N))
     random.shuffle(input_list)
-    if input_list == sorted(input_list):
-        print("Input list is already sorted")
+    output_list = insertion_sort(input_list)
+    if output_list == sorted(output_list):
+        print("List sorted")
     else:
         raise ValueError("Input list is not sorted")
