5482f8fcc91152bf82ef56c8e1dfc2e645de717b
Kaze Wong <kazewong.physics@gmail.com>
2024-07-16 14:20:49 -0400

    Add reference test

--- python/test_sort.py
+++ python/test_sort.py
@@ -8,13 +8,15 @@ def insertion_sort(unsorted_list: list[int]) -> list[int]:
     # Implement your function here ...
     raise NotImplementedError
 
-
 # Do not change the following lines
 def test_insertion_sort():
     input_list = list(range(N))
     random.shuffle(input_list)
     output_list = insertion_sort(input_list)
-    if output_list == sorted(output_list):
-        print("List sorted")
-    else:
-        raise ValueError("Input list is not sorted")
+    assert output_list == sorted(output_list), "Input list is not sorted"
+
+def test_reference():
+    input_list = list(range(N))
+    random.shuffle(input_list)
+    output_list = sorted(input_list)
+    assert output_list == sorted(output_list), "Input list is not sorted"
